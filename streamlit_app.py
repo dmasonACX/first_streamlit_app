7f9da7d21b177583d383da85eb67b9379fe36003
dmasonACX <138720631+dmasonACX@users.noreply.github.com>
2023-07-07 11:56:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+##### FUNCTION
+def get_fruityvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -31,16 +37,17 @@ try:
       streamlit.error("Please select a fruit to get information.")
       #######streamlit.write('The user entered ', fruit_choice)
    else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       # streamlit.text(fruityvice_response.json()) #just writes data to the screen
 
 
       #   write your own comment -what does the next line do? 
       # This generates a table using the pandas library
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       # write your own comment - what does this do?
       # This puts the normalized data into a nice table on the screen
-      streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 except URLError as e:
       streamlit.error()
 
